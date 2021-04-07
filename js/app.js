2a30158757eb11ef7d54d4774cfeaa67a3548774
nicole-tuznik <69260034+nicole-tuznik@users.noreply.github.com>
2021-04-07 14:32:19 -0400

    Final styling

--- js/app.js
+++ js/app.js
@@ -2,11 +2,11 @@ const hamburger = document.querySelector(".nav__hamburger");
 const nav = document.querySelector(".nav__ul");
 
 hamburger.addEventListener("click", function () {
-  if (nav.style.display === "none") {
-    nav.style.display = "block";
+  nav.classList.toggle("show-menu");
+
+  if ((hamburger.src = "/images/icon-hamburger.svg")) {
     hamburger.src = "/images/icon-close.svg";
   } else {
-    nav.style.display = "none";
     hamburger.src = "/images/icon-hamburger.svg";
   }
 });
